fb18487fdd9222ea37804d521a65fbb1d3cd23ae
Daria Timoshevskaya <vileonika@gmail.com>
2023-02-05 17:16:48 +0300

    step 1

---
+++ Makefile
@@ -0,0 +1,5 @@
+gendiff:
+	node bin/gendiff.js
+
+publish:
+	npm publish --dry-run
---
+++ bin/gendiff.js
@@ -0,0 +1,12 @@
+#!/usr/bin/env node
+
+import { Command } from 'commander';
+
+const program = new Command();
+
+program
+    .version('0.0.1')
+    .description('Compares two configuration files and shows a difference')
+    .option('-h, --help', 'display help for command')
+
+program.parse();
